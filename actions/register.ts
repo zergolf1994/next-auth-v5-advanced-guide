2b4751be4f88f8ec363f75fcc915256a0d49e0ee
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-12-29 02:13:19 +0100

    12: resend

--- actions/register.ts
+++ actions/register.ts
@@ -6,6 +6,8 @@ import bcrypt from "bcryptjs";
 import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import { sendVerificationEmail } from "@/lib/mail";
+import { generateVerificationToken } from "@/lib/tokens";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -31,7 +33,11 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     },
   });
 
-  // TODO: Send verification token email
+  const verificationToken = await generateVerificationToken(email);
+  await sendVerificationEmail(
+    verificationToken.email,
+    verificationToken.token,
+  );
 
-  return { success: "User created!" };
+  return { success: "Confirmation email sent!" };
 };
